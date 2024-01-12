e07c14b20df55f64c0b47e8b373bbb5a8b4ad2ad
dongchuliang <s_dongchuliang1@wps.cn>
2024-01-12 16:10:39 +0800

    文件系统

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "hello,world!")
-	})
+	//http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+	//	fmt.Fprintf(w, "hello,world!")
+	//})
+	//
+	//fmt.Println("serve is listening...")
 
-	fmt.Println("serve is listening...")
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
-
 }
