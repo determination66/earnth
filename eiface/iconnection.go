3d6aed22da6bb15c3a48f836b75f9718b6ff7d4f
董楚梁 <2496417370@qq.com>
2024-01-16 01:13:37 +0800

    feat:connection的初步设计

--- eiface/iconnection.go
+++ eiface/iconnection.go
@@ -4,16 +4,19 @@ import "net"
 
 // IConnection 定义连接模块的接口
 type IConnection interface {
-	//Start 启动连接
+	// Start 启动连接
 	Start()
-	//Stop 停止连接
+	// Stop 停止连接
 	Stop()
-	//GetTcpConnection 获取当前连接的socket conn
+	// GetTcpConnection 获取当前连接的socket conn
 	GetTcpConnection() *net.TCPConn
 	//GetConnId 获取远程的tcp状态
-	GetConnId() net.Addr
-	//Send 发送数据，发送给客户端
+	GetConnId() uint32
+	// RemoteAddr 获取远程客户端TCP状态
+	RemoteAddr() net.Addr
+	// Send 发送数据，发送给客户端
 	Send(data []byte) error
 }
 
+// HandleFunc 业务处理
 type HandleFunc func(*net.TCPConn, []byte, int) error
--- enet/connection.go
+++ enet/connection.go
@@ -1,1 +1,33 @@
 package enet
+
+import (
+	"earnth/eiface"
+	"net"
+)
+
+type Connection struct {
+	// 当前tcp套接字
+	Conn *net.TCPConn
+
+	//连接ID
+	ConnID uint32
+
+	//当前连接状态
+	isClose bool
+
+	//当前连接所绑定的业务方法API
+	handleAPI eiface.HandleFunc
+
+	//告知当前连接已经退出
+	ExitChan chan bool
+}
+
+func NewConnection(conn *net.TCPConn, connID uint32, callbackAPI eiface.HandleFunc) *Connection {
+	return &Connection{
+		Conn:      conn,
+		ConnID:    connID,
+		isClose:   false,
+		handleAPI: callbackAPI,
+		ExitChan:  make(chan bool, 1),
+	}
+}
