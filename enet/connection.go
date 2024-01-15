92611dcd8858f2700005a6ed85ebaa5c6d607c8f
董楚梁 <2496417370@qq.com>
2024-01-16 01:28:41 +0800

    feat:初步编写完connection模块

--- enet/connection.go
+++ enet/connection.go
@@ -2,6 +2,7 @@ package enet
 
 import (
 	"earnth/eiface"
+	"fmt"
 	"net"
 )
 
@@ -30,4 +31,63 @@ func NewConnection(conn *net.TCPConn, connID uint32, callbackAPI eiface.HandleFu
 		handleAPI: callbackAPI,
 		ExitChan:  make(chan bool, 1),
 	}
+}
+
+func (c *Connection) StartReader() {
+	fmt.Println("Reader goroutine is running...")
+	defer fmt.Println("ConnId=", c, "reader exit ,remote Addr is:", c.RemoteAddr().String())
+	defer c.Stop()
+
+	for {
+		//读取客户端数据到buf
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("recv buf err:", err)
+			continue
+		}
+		//调用当前连接绑定的HandlAPI
+		err = c.handleAPI(c.Conn, buf, cnt)
+		if err != nil {
+			fmt.Println("ConnID", c.ConnID, " handle err:", err)
+			break
+		}
+
+	}
+}
+
+func (c *Connection) Start() {
+	fmt.Println("Conn Start()... ConnID", c.ConnID)
+	//启动从当前读数据业务
+	//todo 启动写数据业务
+	go c.StartReader()
+
+}
+
+func (c *Connection) Stop() {
+	fmt.Println("Conn Stop()... ConnID =", c.ConnID)
+	if c.isClose == true {
+		return
+	}
+	c.isClose = true
+
+	c.Conn.Close()
+	close(c.ExitChan)
+}
+
+func (c *Connection) GetTcpConnection() *net.TCPConn {
+	return c.Conn
+}
+
+func (c *Connection) GetConnId() uint32 {
+	return c.ConnID
+}
+
+func (c *Connection) RemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
+}
+
+func (c *Connection) Send(data []byte) error {
+	//TODO implement me
+	panic("implement me")
 }
