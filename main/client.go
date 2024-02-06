f0fc68d2b136d7445e6860ddce89fd85d1dec307
dongchuliang <2496417370@qq.com>
2024-02-06 09:32:41 +0800

    chore:测试完毕，还原之前的client、server

--- main/client.go
+++ main/client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"earnth/enet"
 	"fmt"
 	"net"
 	"time"
@@ -18,55 +17,21 @@ func main() {
 		fmt.Println("net dail err:", err)
 		return
 	}
-	//创建一个封包对象 dp
-	dp := enet.NewDataPack()
 
-	//封装一个msg1包
-	msg1 := &enet.Message{
-		Id:      0,
-		DataLen: 5,
-		Data:    []byte{'h', 'e', 'l', 'l', 'o'},
+	for {
+		_, err := conn.Write([]byte("hello,world!"))
+		if err != nil {
+			fmt.Println("conn write err:", err)
+			return
+		}
+		buf := make([]byte, 512)
+		cnt, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn read err:", err)
+			return
+		}
+		fmt.Println("server call back:", string(buf), ", cnt:", cnt)
+		time.Sleep(time.Second)
 	}
-
-	sendData1, err := dp.Pack(msg1)
-	if err != nil {
-		fmt.Println("client pack msg1 err:", err)
-		return
-	}
-
-	msg2 := &enet.Message{
-		Id:      1,
-		DataLen: 7,
-		Data:    []byte{'w', 'o', 'r', 'l', 'd', '!', '!'},
-	}
-	sendData2, err := dp.Pack(msg2)
-	if err != nil {
-		fmt.Println("client temp msg2 err:", err)
-		return
-	}
-
-	//将sendData1，和 sendData2 拼接一起，组成粘包
-	sendData1 = append(sendData1, sendData2...)
-
-	//向服务器端写数据
-	conn.Write(sendData1)
-
-	//客户端阻塞
-	select {}
-	//for {
-	//	_, err := conn.Write([]byte("hello,world!"))
-	//	if err != nil {
-	//		fmt.Println("conn write err:", err)
-	//		return
-	//	}
-	//	buf := make([]byte, 512)
-	//	cnt, err := conn.Read(buf)
-	//	if err != nil {
-	//		fmt.Println("conn read err:", err)
-	//		return
-	//	}
-	//	fmt.Println("server call back:", string(buf), ", cnt:", cnt)
-	//	time.Sleep(time.Second)
-	//}
 
 }
--- main/server.go
+++ main/server.go
@@ -4,8 +4,6 @@ import (
 	"earnth/eiface"
 	"earnth/enet"
 	"fmt"
-	"io"
-	"net"
 )
 
 type PingRouter struct {
@@ -35,59 +33,7 @@ func (pr *PingRouter) PostHandle(req eiface.IRequest) {
 }
 
 func main() {
-	//s := enet.NewServer()
-	//s.AddRouter(&PingRouter{})
-	//s.Serve()
-
-	//创建socket TCP Server
-	listener, err := net.Listen("tcp", "127.0.0.1:8888")
-	if err != nil {
-		fmt.Println("server listen err:", err)
-		return
-	}
-
-	//创建服务器gotoutine，负责从客户端goroutine读取粘包的数据，然后进行解析
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("server accept err:", err)
-		}
-
-		//处理客户端请求
-		go func(conn net.Conn) {
-			//创建封包拆包对象dp
-			dp := enet.NewDataPack()
-			for {
-				//1 先读出流中的head部分
-				headData := make([]byte, dp.GetHeadLen())
-				_, err := io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
-				if err != nil {
-					fmt.Println("read head error")
-					break
-				}
-				//将headData字节流 拆包到msg中
-				msgHead, err := dp.UnPack(headData)
-				if err != nil {
-					fmt.Println("server unpack err:", err)
-					return
-				}
-
-				if msgHead.GetDataLen() > 0 {
-					//msg 是有data数据的，需要再次读取data数据
-					msg := msgHead.(*enet.Message)
-					msg.Data = make([]byte, msg.GetDataLen())
-
-					//根据dataLen从io中读取字节流
-					_, err := io.ReadFull(conn, msg.Data)
-					if err != nil {
-						fmt.Println("server unpack data err:", err)
-						return
-					}
-
-					fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", string(msg.Data))
-				}
-			}
-		}(conn)
-	}
+	s := enet.NewServer()
+	s.AddRouter(&PingRouter{})
+	s.Serve()
 }
