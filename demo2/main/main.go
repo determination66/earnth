8323604946a8f3ac8d1fa4b966c99e92432b47f4
dongchuliang <2496417370@qq.com>
2024-04-03 10:29:36 +0800

    feat:修改实现方式

--- demo2/main/main.go
+++ demo2/main/main.go
@@ -5,18 +5,22 @@ import (
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-
+type Engine struct {
 }
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	case "/index":
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, r.URL)
-	})
-
-	http.HandleFunc("/index", indexHandler)
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", &Engine{})
 }
