8d018238ff0576fb571958a0c1b7191d8ecc70d2
dongchuliang <2496417370@qq.com>
2024-04-18 18:50:44 +0800

    feat:添加接口

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package earnth
 
-import "net/http"
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
 
 type HandleFunc func()
 
+var _ Server = &HTTPServer{}
+
 type Server interface {
 	http.Handler
-	Start()
+	Start(addr string) error
 
 	// AddRoute 也就是说可以用GET、POST、DELETE、OPTIONS、PUT、TRACE、CONNECT、HEAD
 	AddRoute(method string, path string, handleFunc HandleFunc)
@@ -20,16 +26,50 @@ func NewHTTPServer() *HTTPServer {
 }
 
 func (H *HTTPServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	//TODO implement me
-	panic("implement me")
+	// 处理业务。
+
+	//fmt.Println("serveHTTP")
 }
 
-func (H *HTTPServer) Start() {
-	//TODO implement me
-	panic("implement me")
+func (H *HTTPServer) Start(addr string) error {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	return http.Serve(ln, H)
 }
 
 func (H *HTTPServer) AddRoute(method string, path string, handleFunc HandleFunc) {
-	//TODO implement me
-	panic("implement me")
+	fmt.Println("add route method:", method, "path:", path)
+}
+
+func (H *HTTPServer) Get(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodGet, path, handleFunc)
+}
+
+func (H *HTTPServer) Post(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodPost, path, handleFunc)
+}
+
+func (H *HTTPServer) Options(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodOptions, path, handleFunc)
+}
+
+func (H *HTTPServer) Delete(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodDelete, path, handleFunc)
+}
+
+func (H *HTTPServer) Put(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodPut, path, handleFunc)
+}
+
+func (H *HTTPServer) Patch(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodPatch, path, handleFunc)
+}
+
+func (H *HTTPServer) Head(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodHead, path, handleFunc)
+}
+func (H *HTTPServer) Trace(path string, handleFunc HandleFunc) {
+	H.AddRoute(http.MethodTrace, path, handleFunc)
 }
--- server_test.go
+++ server_test.go
@@ -1,1 +1,17 @@
 package earnth
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestServer(T *testing.T) {
+	s := NewHTTPServer()
+	//fmt.Println(s)
+
+	s.Get("/user", func() {
+		fmt.Println("hello world")
+	})
+
+	s.Start(":8080")
+}
